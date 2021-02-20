835029be10d1d3baa58f4b95955b701d49f4c92e
kainDoneson@gmail.com <kaindoneson@gmail.com>
2021-02-19 19:57:20 -0500

    wrapper did not work trying css trick now

--- src/pages/testPage.tsx
+++ src/pages/testPage.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ViewMap from '../components/view-map/ViewMap';
-import Div100vh from 'react-div-100vh';
 
 const testPage: React.FC = () => {
     return (
         <>
-            <Div100vh>
-                <ViewMap
-                    googleMapURL={`https://maps.google.com/maps/api/js?key=${process.env.REACT_APP_GOOGK}`}
-                    loadingElement={<div className="loading-element" />}
-                    containerElement={<div className="view-map" />}
-                    mapElement={<div className="map-element" />}
-                />
-            </Div100vh>
+            <ViewMap
+                googleMapURL={`https://maps.google.com/maps/api/js?key=${process.env.REACT_APP_GOOGK}`}
+                loadingElement={<div className="loading-element" />}
+                containerElement={<div className="view-map" />}
+                mapElement={<div className="map-element" />}
+            />
         </>
     );
 };
