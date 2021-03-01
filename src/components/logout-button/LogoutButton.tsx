4d646688bf155e12e1740bab8cb8836b42a67e40
kainDoneson@gmail.com <kaindoneson@gmail.com>
2021-03-01 13:42:28 -0500

    fixed logout

--- src/components/logout-button/LogoutButton.tsx
+++ src/components/logout-button/LogoutButton.tsx
@@ -8,7 +8,7 @@ const LogoutButton: React.FC = () => {
             className="btn btn-danger btn-block"
             onClick={() =>
                 logout({
-                    returnTo: 'https://scavenger-frontend.vercel.app/home',
+                    returnTo: 'https://scavenger-frontend.vercel.app/',
                 })
             }
         >
