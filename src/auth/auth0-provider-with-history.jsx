38250004472d8ae122c909bd6023a537b7b81f44
kainDoneson@gmail.com <kaindoneson@gmail.com>
2021-03-01 13:34:34 -0500

    test10

--- src/auth/auth0-provider-with-history.jsx
+++ src/auth/auth0-provider-with-history.jsx
@@ -17,7 +17,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
         <Auth0Provider
             domain={domain}
             clientId={clientId}
-            redirectUri={window.location.origin}
+            redirectUri="https://scavenger-frontend.vercel.app/home"
             //onRedirectCallback={onRedirectCallback}
         >
             {children}
