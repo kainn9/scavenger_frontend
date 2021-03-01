2a9bf62838384b473e9d3f484821fa30c2646899
kainDoneson@gmail.com <kaindoneson@gmail.com>
2021-03-01 00:24:43 -0500

    test07

--- src/components/spot-player-w-loader/SpotPlayerLoader.tsx
+++ src/components/spot-player-w-loader/SpotPlayerLoader.tsx
@@ -4,9 +4,8 @@ import SpotifyPlayer from 'react-spotify-player';
 
 interface Props {
     uri: string;
-    isSong?: boolean;
 }
-const SpotPlayerLoader: React.FC<Props> = function ({ uri, isSong }) {
+const SpotPlayerLoader: React.FC<Props> = function ({ uri }) {
     const [isMounted, setIsMounted] = useState<boolean>(false);
     const [uriSwap, setUriSwap] = useState<string>('spotify:album:27ftYHLeunzcSzb33Wk1hf');
     useEffect(() => {
