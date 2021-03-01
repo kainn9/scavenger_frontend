780ec2918a62301b83ddc658a3185225fe59badc
kainDoneson@gmail.com <kaindoneson@gmail.com>
2021-03-01 14:29:23 -0500

    cl marker test

--- src/components/current-location-marker/CurrentLocationMarker.tsx
+++ src/components/current-location-marker/CurrentLocationMarker.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { CLRootState, Action, currentLocation } from '../../redux/current-location/currentLocationReducer';
 import { PLACE_CL_MARKER } from '../../redux/current-location/currentLocationActions';
+import clIcon from '../../assets/cl-icon.png';
 
 // getting props.currentLocation from redux
 const msp = ({ currentLocation }: { currentLocation: CLRootState }) => ({
@@ -23,7 +24,19 @@ const CurrentLocationMarker: React.FC<reduxProps> = function ({ currentLocation,
             PLACE_CL_MARKER({ lat: coords.latitude, lng: coords.longitude }),
         );
     }, []);
-    return currentLocation ? <Marker position={{ lat: currentLocation.lat, lng: currentLocation.lng }} /> : null;
+    return currentLocation ? (
+        <Marker
+            position={{ lat: currentLocation.lat, lng: currentLocation.lng }}
+            label="You"
+            icon={{
+                url: clIcon,
+                scaledSize: new window.google.maps.Size(40, 40),
+                origin: new window.google.maps.Point(0, 0),
+                //anchor: new window.google.maps.Point(0, 0),
+                labelOrigin: new window.google.maps.Point(20, 47),
+            }}
+        />
+    ) : null;
 };
 
 export default connector(CurrentLocationMarker);
