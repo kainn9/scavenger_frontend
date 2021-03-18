e9dc3fa253af997cbcdb377cdbd198f29a3f8ce5
kainDoneson@gmail.com <kaindoneson@gmail.com>
2021-03-18 02:20:51 -0400

    small bug fix on preview map, now clears previewNode when switching routes

--- src/components/route-search-preview/RouteSearchResultPreview.tsx
+++ src/components/route-search-preview/RouteSearchResultPreview.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { Icon } from 'semantic-ui-react';
-import { SET_ACTIVE_ROUTE } from '../../redux/active-route/activeRouteActions';
+import { SET_ACTIVE_NODE, SET_ACTIVE_ROUTE } from '../../redux/active-route/activeRouteActions';
 import { Action, activeRoute, backendRoute } from '../../redux/active-route/activeRouteReducer';
 import './RouteSearchResultPreviewStyles.scss';
 
 //redux
 const mdp = (dispatch: (action: Action) => void) => ({
     SET_ACTIVE_ROUTE: (route: activeRoute | null) => dispatch(SET_ACTIVE_ROUTE(route)),
+    SET_ACTIVE_NODE: (n: null) => dispatch(SET_ACTIVE_NODE(n)),
 });
 const connector = connect(null, mdp);
 type ReduxProps = ConnectedProps<typeof connector>;
 interface props extends ReduxProps {
     route: backendRoute;
 }
-const RouteSearchResultPreview: React.FC<props> = function ({ route, SET_ACTIVE_ROUTE }) {
+const RouteSearchResultPreview: React.FC<props> = function ({ route, SET_ACTIVE_ROUTE, SET_ACTIVE_NODE }) {
     console.log(route);
     return (
-        <div className="route-s-result" onClick={() => SET_ACTIVE_ROUTE(route.nodes)}>
+        <div
+            className="route-s-result"
+            onClick={() => {
+                SET_ACTIVE_NODE(null);
+                SET_ACTIVE_ROUTE(route.nodes);
+            }}
+        >
             <h1>{route.title}</h1>
             <p>Node Count: {route.nodes.length}</p>
             <Icon className="map" />
